196966d60855b95c699743fa1bb2e4621cc795f8
marcooly-bit <marcoolivotto46@gmail.com>
2026-09-10 17:30:08 +0200

    Update players.js

--- data/players.js
+++ data/players.js
@@ -1,1 +1,441 @@
+```js
+const players = [
+
+  // =========================
+  // PORTIERI
+  // =========================
+
+  {
+    name: "Kamil Grabara",
+    team: "Juventus",
+    number: 1,
+    position: "Portiere",
+    image: "images/players/grabara.png"
+  },
+
+  {
+    name: "Carlo Pinsoglio",
+    team: "Juventus",
+    number: 23,
+    position: "Portiere",
+    image: "images/players/pinsoglio.png"
+  },
+
+  {
+    name: "Guglielmo Vicario",
+    team: "Juventus",
+    number: 25,
+    position: "Portiere",
+    image: "images/players/vicario.png"
+  },
+
+  {
+    name: "Josep Martínez",
+    team: "Inter",
+    number: 1,
+    position: "Portiere",
+    image: "images/players/josep-martinez.png"
+  },
+
+  {
+    name: "Raffaele Di Gennaro",
+    team: "Inter",
+    number: 12,
+    position: "Portiere",
+    image: "images/players/di-gennaro.png"
+  },
+
+  {
+    name: "Ivan Provedel",
+    team: "Inter",
+    number: 49,
+    position: "Portiere",
+    image: "images/players/provedel.png"
+  },
+
+
+  // =========================
+  // DIFENSORI
+  // =========================
+
+  {
+    name: "Bremer",
+    team: "Juventus",
+    number: 3,
+    position: "Difensore",
+    image: "images/players/bremer.png"
+  },
+
+  {
+    name: "Federico Gatti",
+    team: "Juventus",
+    number: 4,
+    position: "Difensore",
+    image: "images/players/gatti.png"
+  },
+
+  {
+    name: "Lloyd Kelly",
+    team: "Juventus",
+    number: 6,
+    position: "Difensore",
+    image: "images/players/kelly.png"
+  },
+
+  {
+    name: "Pierre Kalulu",
+    team: "Juventus",
+    number: 15,
+    position: "Difensore",
+    image: "images/players/kalulu.png"
+  },
+
+  {
+    name: "Andrea Cambiaso",
+    team: "Juventus",
+    number: 20,
+    position: "Difensore",
+    image: "images/players/cambiaso.png"
+  },
+
+  {
+    name: "Daniele Rugani",
+    team: "Juventus",
+    number: 24,
+    position: "Difensore",
+    image: "images/players/rugani.png"
+  },
+
+  {
+    name: "Jhon Lucumí",
+    team: "Juventus",
+    number: 26,
+    position: "Difensore",
+    image: "images/players/lucumi.png"
+  },
+
+  {
+    name: "Juan David Cabal",
+    team: "Juventus",
+    number: 32,
+    position: "Difensore",
+    image: "images/players/cabal.png"
+  },
+
+  {
+    name: "Zeki Çelik",
+    team: "Juventus",
+    number: 2,
+    position: "Difensore",
+    image: "images/players/celik.png"
+  },
+
+  {
+    name: "John Stones",
+    team: "Inter",
+    number: 6,
+    position: "Difensore",
+    image: "images/players/john-stones.png"
+  },
+
+  {
+    name: "Manuel Akanji",
+    team: "Inter",
+    number: 25,
+    position: "Difensore",
+    image: "images/players/akanji.png"
+  },
+
+  {
+    name: "Benjamin Pavard",
+    team: "Inter",
+    number: 28,
+    position: "Difensore",
+    image: "images/players/pavard.png"
+  },
+
+  {
+    name: "Carlos Augusto",
+    team: "Inter",
+    number: 30,
+    position: "Difensore",
+    image: "images/players/carlos-augusto.png"
+  },
+
+  {
+    name: "Yann Bisseck",
+    team: "Inter",
+    number: 31,
+    position: "Difensore",
+    image: "images/players/bisseck.png"
+  },
+
+  {
+    name: "Federico Dimarco",
+    team: "Inter",
+    number: 32,
+    position: "Difensore",
+    image: "images/players/dimarco.png"
+  },
+
+  {
+    name: "Alessandro Bastoni",
+    team: "Inter",
+    number: 95,
+    position: "Difensore",
+    image: "images/players/bastoni.png"
+  },
+
+  {
+    name: "Djed Spence",
+    team: "Inter",
+    number: 99,
+    position: "Difensore",
+    image: "images/players/spence.png"
+  },
+
+
+  // =========================
+  // CENTROCAMPISTI
+  // =========================
+
+  {
+    name: "Manuel Locatelli",
+    team: "Juventus",
+    number: 5,
+    position: "Centrocampista",
+    image: "images/players/locatelli.png"
+  },
+
+  {
+    name: "Teun Koopmeiners",
+    team: "Juventus",
+    number: 8,
+    position: "Centrocampista",
+    image: "images/players/koopmeiners.png"
+  },
+
+  {
+    name: "Douglas Luiz",
+    team: "Juventus",
+    number: 12,
+    position: "Centrocampista",
+    image: "images/players/douglas-luiz.png"
+  },
+
+  {
+    name: "Kerim Alajbegović",
+    team: "Juventus",
+    number: 17,
+    position: "Centrocampista",
+    image: "images/players/alajbegovic.png"
+  },
+
+  {
+    name: "Khéphren Thuram",
+    team: "Juventus",
+    number: 19,
+    position: "Centrocampista",
+    image: "images/players/thuram-k.png"
+  },
+
+  {
+    name: "Weston McKennie",
+    team: "Juventus",
+    number: 22,
+    position: "Centrocampista",
+    image: "images/players/mckennie.png"
+  },
+
+  {
+    name: "Pape Matar Sarr",
+    team: "Juventus",
+    number: 29,
+    position: "Centrocampista",
+    image: "images/players/sarr.png"
+  },
+
+  {
+    name: "Nicolás González",
+    team: "Juventus",
+    number: 31,
+    position: "Centrocampista",
+    image: "images/players/nicolas-gonzalez.png"
+  },
+
+  {
+    name: "Aleksandar Stanković",
+    team: "Inter",
+    number: 5,
+    position: "Centrocampista",
+    image: "images/players/stankovic.png"
+  },
+
+  {
+    name: "Piotr Zielinski",
+    team: "Inter",
+    number: 7,
+    position: "Centrocampista",
+    image: "images/players/zielinski.png"
+  },
+
+  {
+    name: "Petar Sučić",
+    team: "Inter",
+    number: 8,
+    position: "Centrocampista",
+    image: "images/players/sucic.png"
+  },
+
+  {
+    name: "Andy Diouf",
+    team: "Inter",
+    number: 17,
+    position: "Centrocampista",
+    image: "images/players/diouf.png"
+  },
+
+  {
+    name: "Hakan Çalhanoğlu",
+    team: "Inter",
+    number: 20,
+    position: "Centrocampista",
+    image: "images/players/calhanoglu.png"
+  },
+
+  {
+    name: "Curtis Jones",
+    team: "Inter",
+    number: 21,
+    position: "Centrocampista",
+    image: "images/players/curtis-jones.png"
+  },
+
+  {
+    name: "Henrikh Mkhitaryan",
+    team: "Inter",
+    number: 22,
+    position: "Centrocampista",
+    image: "images/players/mkhitaryan.png"
+  },
+
+  {
+    name: "Nicolò Barella",
+    team: "Inter",
+    number: 23,
+    position: "Centrocampista",
+    image: "images/players/barella.png"
+  },
+
+
+  // =========================
+  // ATTACCANTI
+  // =========================
+
+  {
+    name: "Francisco Conceição",
+    team: "Juventus",
+    number: 7,
+    position: "Attaccante",
+    image: "images/players/conceicao.png"
+  },
+
+  {
+    name: "Randal Kolo Muani",
+    team: "Juventus",
+    number: 9,
+    position: "Attaccante",
+    image: "images/players/kolo-muani.png"
+  },
+
+  {
+    name: "Kenan Yildiz",
+    team: "Juventus",
+    number: 10,
+    position: "Attaccante",
+    image: "images/players/yildiz.png"
+  },
+
+  {
+    name: "Edon Zhegrova",
+    team: "Juventus",
+    number: 11,
+    position: "Attaccante",
+    image: "images/players/zhegrova.png"
+  },
+
+  {
+    name: "Jérémie Boga",
+    team: "Juventus",
+    number: 13,
+    position: "Attaccante",
+    image: "images/players/boga.png"
+  },
+
+  {
+    name: "Arkadiusz Milik",
+    team: "Juventus",
+    number: 14,
+    position: "Attaccante",
+    image: "images/players/milik.png"
+  },
+
+  {
+    name: "Jeff Ekhator",
+    team: "Juventus",
+    number: 18,
+    position: "Attaccante",
+    image: "images/players/ekhator.png"
+  },
+
+  {
+    name: "Nick Woltemade",
+    team: "Juventus",
+    number: 27,
+    position: "Attaccante",
+    image: "images/players/woltemade.png"
+  },
+
+  {
+    name: "Marcus Thuram",
+    team: "Inter",
+    number: 9,
+    position: "Attaccante",
+    image: "images/players/marcus-thuram.png"
+  },
+
+  {
+    name: "Lautaro Martínez",
+    team: "Inter",
+    number: 10,
+    position: "Attaccante",
+    image: "images/players/lautaro.png"
+  },
+
+  {
+    name: "Luis Henrique",
+    team: "Inter",
+    number: 11,
+    position: "Attaccante",
+    image: "images/players/luis-henrique.png"
+  },
+
+  {
+    name: "Ange-Yoan Bonny",
+    team: "Inter",
+    number: 14,
+    position: "Attaccante",
+    image: "images/players/bonny.png"
+  },
+
+  {
+    name: "Pio Esposito",
+    team: "Inter",
+    number: 94,
+    position: "Attaccante",
+    image: "images/players/pio-esposito.png"
+  }
+
+];
+```
 
